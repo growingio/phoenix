b41207eb1b1ba11bbd3d7f139662c45ea74b5769
Shuoliu <growingio@ip-192-168-55-227.cn-north-1.compute.internal>
2019-07-30 18:00:58 +0800

    fix case when work on union subsql

--- phoenix-core/src/main/java/org/apache/phoenix/expression/ComparisonExpression.java
+++ phoenix-core/src/main/java/org/apache/phoenix/expression/ComparisonExpression.java
@@ -245,7 +245,7 @@ public class ComparisonExpression extends BaseCompoundExpression {
 
             // Determine if we know the expression must be TRUE or FALSE based on the max size of
             // a fixed length expression.
-            if (children.get(1).getMaxLength() != null && lhsExpr.getMaxLength() != null && lhsExpr.getMaxLength() < children.get(1).getMaxLength()) {
+            if (children.get(1).getMaxLength() != null && lhsExpr.getMaxLength() != null && lhsExprDataType.isFixedWidth() && lhsExpr.getMaxLength() < children.get(1).getMaxLength() ) {
                 switch (op) {
                 case EQUAL:
                     return LiteralExpression.newConstant(false, PBoolean.INSTANCE, determinism);
