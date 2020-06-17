4268442c8300fac86d86759b3bb5a5f2fd6e0172
gaoxianglin <872980156@qq.com>
2020-06-17 16:04:41 +0800

    增加注释

--- phoenix-core/src/main/java/org/apache/phoenix/expression/aggregator/gio/BitMapAggregator.java
+++ phoenix-core/src/main/java/org/apache/phoenix/expression/aggregator/gio/BitMapAggregator.java
@@ -13,16 +13,26 @@ public class BitMapAggregator {
         return new Integer(high8Bit | low8Bit).shortValue();
     }
 
-    // 在rule_id不足128的时候，给lowBit9位，在rule_id超过128的时候，都是8位
+    /**
+     * 动态调整rid 和 bucketId需要的位
+     * leftShiftBit 是rid的左移位，根据ridCount, 只给rid的index留够正好的位就可以了
+     * 比如，传入5个rid, 那么tableSizeFor(5) = 8,8的二进制就是1000, 是2的三次方
+     * rid一定小于8是 0 - 7的值，最大111,用三位就可以保存，所以 16 - 3 = 13，rid的数据左移13位，剩下的位给bucketId使用
+     * @param rid rule_id的index
+     * @param bucketId
+     * @param ridCount 合并指标中rule_id的个数
+     * @return 新编号
+     */
     public static short mergeRid3(short rid, short bucketId, int ridCount) {
         int leftShiftBit = (int) Math.max(16 - (Math.log(tableSizeFor(ridCount)) / Math.log(2)), 8);
-        int bucketBaseBit = Math.max(maxBit / tableSizeFor(ridCount), eightBit);
-        int ridBaseBit = tableSizeFor(ridCount) - 1;
-        int high8Bit = (rid & ridBaseBit) << leftShiftBit;
-        int low8Bit = (bucketId & bucketBaseBit);
-        return new Integer(high8Bit | low8Bit).shortValue();
+        int bucketBaseBit = Math.max((maxBit / tableSizeFor(ridCount)) - 1, eightBit);
+        int ridBaseBit = Math.min(tableSizeFor(ridCount) - 1, eightBit);
+        int highBit = (rid & ridBaseBit) << leftShiftBit;
+        int lowBit = (bucketId & bucketBaseBit);
+        return new Integer(highBit | lowBit).shortValue();
     }
 
+    // 返回大于输入参数且最近的2的整数次幂的数, hashMap里的tableSizeFor方法
     static int tableSizeFor(int cap) {
         int n = cap - 1;
         n |= n >>> 1;
